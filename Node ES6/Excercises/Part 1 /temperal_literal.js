9f969f316cf7be37198bca8fe1b7a168e208be77
KelvinRod9331 <kelvinrodriguez@ac.c4q.nyc>
2017-11-13 14:13:54 -0500

    First Excercise

---
+++ Node ES6/Excercises/Part 1 /temperal_literal.js
@@ -0,0 +1,7 @@
+function makeStory(name){
+    console.log(`Remember, go straight to Grandma's house," ${name}'s mother cautioned.  
+    "Don't dawdle along the way and please don't talk to strangers!  The woods are dangerous."
+    "Don't worry, mommy," said  ${name}, "I'll be careful.`)
+}
+
+makeStory("kelvin")
